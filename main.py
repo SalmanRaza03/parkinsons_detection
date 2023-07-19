5aae9828be0722664cdd7ec1dd54d431fd9a70f3
Mohammad Salman Raza <135103272+SalmanRaza03@users.noreply.github.com>
2023-07-19 10:37:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Original file is located at
 
 from fastapi import FastAPI
 
-from fast.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pickle
 import json
